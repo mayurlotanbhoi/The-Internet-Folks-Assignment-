878d3d2e7c0630eae80f6eff1033506b99d354b0
mayurlotanbhoi <mayurbhoi200@gmail.com>
2023-02-17 14:38:47 +0530

    first commit

---
+++ src/containers/home/RequisitionDetailsForm.tsx
@@ -0,0 +1,131 @@
+'use clint'
+import { Button, Flex, Box } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import FormInput from "../../components/formComponents/FormInput";
+import FormSelect from "../../components/formComponents/FormSelect";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { PageNumbers } from "../../interface/home";
+import { IRequisitionDetails } from "../../interface/forms";
+import { genderOptions, urgencyOptions } from "./constants";
+import { useContext, Dispatch } from "react";
+import { useData } from "./DataProvider"
+
+const RequisitionDetailsForm: React.FC<{
+  handleTab: (n: PageNumbers) => void;
+
+}> = ({ handleTab }) => {
+  const {
+    handleChange,
+    errors,
+    touched,
+    handleBlur,
+    handleSubmit,
+    values,
+    setFieldTouched,
+    setFieldValue,
+    isValid,
+  } = useFormik<IRequisitionDetails>({
+    initialValues: {
+      requisitionTitle: "",
+      noOfOpenings: 0,
+      urgency: "",
+      gender: "",
+
+    },
+
+    validationSchema: Yup.object().shape({
+      requisitionTitle: Yup.string().required("Requisition title is required"),
+      noOfOpenings: Yup.number()
+        .typeError("Enter a valid number")
+        .required("Number of openings is required")
+        .positive("Enter a valid number")
+        .min(1, "Enter a valid number"),
+      urgency: Yup.string().required("Urgency is required"),
+      gender: Yup.string().required("Gender is required"),
+    }),
+
+    onSubmit: (values) => {
+      handleTab(1);
+    },
+  });
+
+  const data = useData()
+  useEffect(() => {
+    let { setState, state } = data;
+    state.requisitionDetails = { ...values }
+    const newState = { ...state }
+
+
+    setState(newState)
+    // console.log(data);
+  }, [values.gender, values.noOfOpenings, values.requisitionTitle, values.urgency])
+
+
+
+
+
+
+
+
+  // console.log(setStatefun())
+
+
+
+
+  return (
+    <Box width="100%" as="form" onSubmit={handleSubmit as any}>
+      <Box width="100%">
+        <FormInput
+          label="Requisition Title"
+          placeholder="Enter requisition title"
+          name="requisitionTitle"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values?.requisitionTitle}
+          error={errors?.requisitionTitle}
+          touched={touched?.requisitionTitle}
+        />
+        <FormInput
+          label="Number of openings"
+          placeholder="Enter number of openings"
+          name="noOfOpenings"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values?.noOfOpenings}
+          error={errors?.noOfOpenings}
+          touched={touched?.noOfOpenings}
+        />
+        <FormSelect
+          label="Gender"
+          name="gender"
+          placeholder="Select gender"
+          options={genderOptions}
+          onChange={setFieldValue}
+          onBlur={setFieldTouched}
+          error={errors.gender}
+          touched={touched.gender}
+          value={values.gender}
+        />
+        <FormSelect
+          label="Urgency"
+          name="urgency"
+          placeholder="Select urgency"
+          options={urgencyOptions}
+          onChange={setFieldValue}
+          onBlur={setFieldTouched}
+          error={errors.urgency}
+          touched={touched.urgency}
+          value={values.urgency}
+        />
+        <Flex w="100%" justify="flex-end" mt="4rem">
+          <Button colorScheme="red" type="submit">
+            Next
+          </Button>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+export default RequisitionDetailsForm;
